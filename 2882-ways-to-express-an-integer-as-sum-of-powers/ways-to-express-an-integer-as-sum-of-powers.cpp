60f414b1fabf4385fccca0b1d6cd4fffca8a249b
AMIT KUMAR PANCHAYAN <166891911+the-terror-wonder@users.noreply.github.com>
2025-08-12 13:32:39 +0530

    Time: 975 ms (6.75%) | Memory: 274.4 MB (30.28%) - LeetSync

--- 2882-ways-to-express-an-integer-as-sum-of-powers/ways-to-express-an-integer-as-sum-of-powers.cpp
+++ 2882-ways-to-express-an-integer-as-sum-of-powers/ways-to-express-an-integer-as-sum-of-powers.cpp
@@ -1,60 +1,33 @@
-#include <vector>
-using namespace std;
-
 class Solution {
-    static constexpr int MOD = 1e9 + 7;
-    static constexpr int MAX_N = 300;
-    static constexpr int MAX_X = 5;
+public:
+    const int MOD = 1e9 + 7;
 
-    // Precomputed answers: precomputed[n][x]
-    static vector<vector<int>> precomputed;
-
-    // Static fast integer power
-    static int ipow(int base, int exp) {
-        int res = 1;
-        while (exp--) res *= base;
-        return res;
+    long long power(long long base, int exp) {
+        long long result = 1;
+        while (exp > 0) {
+            result *= base;
+            exp--;
+        }
+        return result;
     }
 
-    // Build the table once
-    static vector<vector<int>> compute() {
-        vector<vector<int>> ans(MAX_N + 1, vector<int>(MAX_X + 1, 0));
+    int solve(int n, int x, int sum, int last, vector<vector<int>> &dp) {
+        if (sum == n) return 1;
+        if (dp[sum][last] != -1) return dp[sum][last];
 
-        for (int x = 1; x <= MAX_X; ++x) {
-            // dp[s][k] = #ways to make sum = s using exactly k distinct x-th powers
-            vector<vector<int>> dp(MAX_N + 1, vector<int>(MAX_N + 1, 0));
-            dp[0][0] = 1;
-
-            // For each possible i^x
-            for (int i = 1; ; ++i) {
-                int p = ipow(i, x);
-                if (p > MAX_N) break;
-                // 0/1-knapsack style: include this power
-                for (int s = MAX_N; s >= p; --s) {
-                    for (int k = 0; k < MAX_N; ++k) {
-                        dp[s][k+1] = (dp[s][k+1] + dp[s - p][k]) % MOD;
-                    }
-                }
-            }
-
-            // Sum over all k to get total ways for each n
-            for (int s = 0; s <= MAX_N; ++s) {
-                long long total = 0;
-                for (int k = 0; k <= MAX_N; ++k) {
-                    total = (total + dp[s][k]) % MOD;
-                }
-                ans[s][x] = (int)total;
-            }
+        long long ways = 0;
+        for (int next = last + 1; ; next++) {
+            long long num = power(next, x);
+            if (sum + num > n) break;
+            ways = (ways + solve(n, x, sum + num, next, dp)) % MOD;
         }
 
-        return ans;
+        return dp[sum][last] = ways;
     }
 
-public:
     int numberOfWays(int n, int x) {
-        return precomputed[n][x];
+        vector<vector<int>> dp(n + 1, vector<int>(n + 1, -1));
+        return solve(n, x, 0, 0, dp);
     }
 };
 
-// Definition of the static member
-vector<vector<int>> Solution::precomputed = Solution::compute();
