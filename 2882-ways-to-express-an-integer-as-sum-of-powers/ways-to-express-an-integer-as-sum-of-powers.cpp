5232a694b6d15f9ff2dd56925fe8015bf957e4e4
AMIT KUMAR PANCHAYAN <166891911+the-terror-wonder@users.noreply.github.com>
2025-08-12 13:34:29 +0530

    Time: 2986 ms (5.01%) | Memory: 274.6 MB (23.31%) - LeetSync

--- 2882-ways-to-express-an-integer-as-sum-of-powers/ways-to-express-an-integer-as-sum-of-powers.cpp
+++ 2882-ways-to-express-an-integer-as-sum-of-powers/ways-to-express-an-integer-as-sum-of-powers.cpp
@@ -2,22 +2,15 @@ class Solution {
 public:
     const int MOD = 1e9 + 7;
 
-    long long power(long long base, int exp) {
-        long long result = 1;
-        while (exp > 0) {
-            result *= base;
-            exp--;
-        }
-        return result;
-    }
 
     int solve(int n, int x, int sum, int last, vector<vector<int>> &dp) {
         if (sum == n) return 1;
         if (dp[sum][last] != -1) return dp[sum][last];
 
         long long ways = 0;
-        for (int next = last + 1; ; next++) {
-            long long num = power(next, x);
+        int next = last + 1;
+        for (; ; next++) {
+            long long num = pow(next, x);
             if (sum + num > n) break;
             ways = (ways + solve(n, x, sum + num, next, dp)) % MOD;
         }
